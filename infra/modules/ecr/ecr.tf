820e7ef2a9de970d81c174667861bbe503490af9
smirvf <saahirmirvf@gmail.com>
2025-11-15 17:11:21 +0000

    Updating ECR for GitHub Actions

---
+++ infra/modules/ecr/ecr.tf
@@ -0,0 +1,33 @@
+resource "aws_ecr_repository" "threat_app_image" {
+  name                 = "threat_app_image"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "threat_image_policy" {
+  repository = aws_ecr_repository.threat_app_image.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep the 5 latest images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
--- infra/modules/ecr/outputs.tf
+++ infra/modules/ecr/outputs.tf
@@ -1,14 +1,3 @@
-# output "ec2ip" {
-#   value = aws_instance.WordPressEC2.public_ip
-# }
-#
-# output "instance_public_dns" {
-#   value = aws_instance.WordPressEC2.public_dns
-# }
-#
-# output "wp_url" { value = "http://${aws_instance.WordPressEC2.public_ip}" }
-# output "ssh_cmd" { value = "ssh -i testkey ec2-user@${aws_instance.WordPressEC2.public_ip}" }
-
 output "ecr_url" {
-  value = module.ecr.threat
+  value = aws_ecr_repository.threat_app_image.repository_url
 }
