b11ea34420ec4930332096d35e4e139e13760bf5
smirvf <saahirmirvf@gmail.com>
2025-11-15 17:42:45 +0000

    Adding KMS key as the trivy errors really pushed my buttons lool

--- infra/modules/ecr/ecr.tf
+++ infra/modules/ecr/ecr.tf
@@ -1,5 +1,13 @@
-#trivy:ignore:AVD-AWS-0033
-#checkov:skip=CKV_AWS_136:ECR KMS encryption not required for this proj
+resource "aws_kms_key" "ecr_key" {
+  description             = "KMS key for ECR image encryption"
+  deletion_window_in_days = 7    # 7 days grace period if we delete key
+  enable_key_rotation     = true # auto-rotate key every year
+}
+
+resource "aws_kms_alias" "ecr_alias" {
+  name          = "alias/threat-app-ecr-kms-key"
+  target_key_id = aws_kms_key.ecr_key.key_id
+}
 
 resource "aws_ecr_repository" "threat_app_image" {
   name                 = "threat_app_image"
@@ -9,7 +17,8 @@ resource "aws_ecr_repository" "threat_app_image" {
     scan_on_push = true
   }
   encryption_configuration {
-    encryption_type = "AES256"
+    encryption_type = "KMS"
+    kms_key         = aws_kms_alias.ecr_alias.arn
   }
 }
 
