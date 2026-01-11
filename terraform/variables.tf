eec913bbf59e0b4ad624a9ec7920940d279f75c0
smirvf <saahirmirvf@gmail.com>
2026-01-11 20:49:09 +0000

    TF files for main terraform folder

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name" {
+  type        = string
+  description = "s3 bucket name"
+  default     = "testing-saahir-was-here"
+}
+
+variable "region" {
+  type        = string
+  description = "aws region"
+  default     = "eu-west-2"
+}
