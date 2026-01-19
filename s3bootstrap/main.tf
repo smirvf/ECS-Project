408c8d1920b7b48a29e0c207bd11f0ca4fda32e1
smirvf <saahirmirvf@gmail.com>
2026-01-19 23:06:43 +0000

    Adding all files so that I can transfer work computers

--- s3bootstrap/main.tf
+++ s3bootstrap/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,9 +14,39 @@ provider "aws" {
   region = var.region
 }
 
+output "s3_bucket_name" {
+  value = aws_s3_bucket.s3_bucket.bucket
+}
+
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   tags = {
-    Name = "s3_bucket"
+    Name = "s3_bucket_tf_state"
   }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "acl_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
--- terraform/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.bucket_name
-  tags = {
-    Name = var.bucket_name
-  }
-}
