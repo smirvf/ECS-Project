e49801475c5f2fa9e581700638771b35e5c45864
smirvf <saahirmirvf@gmail.com>
2026-01-23 18:50:58 +0000

    Migrating state to s3 backend for s3 bootstrap

--- s3_bootstrap/main.tf
+++ s3_bootstrap/main.tf
@@ -1,11 +1,18 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  }
+  backend "s3" {
+    bucket = "smvf-s3-bucket-test"
+    key    = "ecs-proj/s3-boostrap/terraform.tfstate"
+    region = "eu-west-2"
+    encrypt = true
+    use_lockfile = true
   }
 }
 
