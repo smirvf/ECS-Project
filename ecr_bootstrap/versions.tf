72a7b25fc65cdee4bc019f3eb76667187f786508
smirvf <saahirmirvf@gmail.com>
2026-01-23 18:57:09 +0000

    Migrating state to s3 backend for ecr bootstrap

--- ecr_bootstrap/versions.tf
+++ ecr_bootstrap/versions.tf
@@ -1,12 +1,18 @@
 terraform {
-  required_version = "~> 1"
+  required_version = ">= 1.1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.61.0"
+      version = "~> 6.0"
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    bucket       = "smvf-s3-bucket-test"
+    key          = "ecs-proj/ecr-bootstrap/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
