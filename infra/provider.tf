be0167044e443c7b28af1a58a085ba72a13a15a9
smirvf <saahirmirvf@gmail.com>
2026-01-31 15:14:40 +0000

    creating base infra dir and respective files

---
+++ infra/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "smvf-s3-bucket-test"
+    key          = "ecs-proj/infra/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
