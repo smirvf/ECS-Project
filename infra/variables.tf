25db5ac657402c189dab05d9668e42e00b3757e2
smirvf <saahirmirvf@gmail.com>
2026-02-07 01:00:28 +0000

    removing extra file and assigning var defaults

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,11 @@
 variable "ecs_task_port" {
   type        = number
+  default     = 8080
   description = "port for task"
 }
 
 variable "alb_subnet_public" {
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
   description = "ALB SG id"
 }
